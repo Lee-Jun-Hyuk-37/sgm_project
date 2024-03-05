7de7c192a2da6a9a4e0e623a4ca0b4318efcf1e8
Lee Jun Hyuk <jmjh98@snu.ac.kr>
2024-03-05 22:39:41 +0900

    after 단장님 demo

--- backend.py
+++ backend.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-from time import sleep
 import datetime as dt
 import secrets
 from pathlib import Path
 import json
-import base64
+import re
+import wave
+import io
+import torch
+from scipy.io.wavfile import write as write_wav
+import numpy as np
+from nossl import no_ssl_verification
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from bark.bark import SAMPLE_RATE, generate_audio, preload_models
+import whisper
 
-
-chat_root=Path('chat_logs')
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", max_http_buffer_size=10000000)  # max_size: 10mb
 
 
 @app.route('/')
@@ -21,21 +27,77 @@ def index():
     return render_template('index.html')
 
 
+def load_models():
+    whisper_ = whisper.load_model("base", device='cpu')
+    with no_ssl_verification():
+        nllb_ = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M", device_map="cpu")
+        preload_models()
+    print("====================")
+    print("model load complete!")
+    print("====================")
+    return whisper_, nllb_
+
+
+@socketio.on('load_tokenizer')
+def load_tokenizer(data):
+    language = data["selectedLanguage"]
+    global tokenizer_kor, tokenizer_opp, opp_lang
+    opp_lang = lang_li.index(language)
+    lang = lang_li_nllb[opp_lang]
+    with no_ssl_verification():
+        tokenizer_kor = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M", src_lang="kor_Hang")
+        tokenizer_opp = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M", src_lang=lang)
+    print("====================")
+    print(language)
+    print("tokenizer load complete!")
+    print("====================")
+
+
+def translate_song(text, opp_lang):
+    language = lang_li_nllb[opp_lang]
+    inputs = tokenizer_kor(text, return_tensors="pt").to(device)
+    translated_tokens = nllb_model.generate(**inputs, forced_bos_token_id=tokenizer_kor.lang_code_to_id[language], max_length=512)
+    output = tokenizer_opp.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+    return output
+
+
+def tts(texts, opp_lang):
+    language = lang_li_bark[opp_lang]
+    file_name = secrets.token_urlsafe(8) + ".wav"
+    res = []
+    with no_ssl_verification():
+        for text in texts:
+            audio_array = generate_audio(text, history_prompt=f"v2/{language}_speaker_0")
+            res.append(audio_array)
+        write_wav("static/audios/" + file_name, SAMPLE_RATE, np.concatenate(res))
+    return file_name
+
+
+def song(text, opp_lang):
+    nllb_model.to(device)
+    outputs = []
+    for txt in re.split("[.?,。]", text):
+        if txt.rstrip() == "" or txt.rstrip() == "\n":
+            continue
+        output = translate_song(txt, opp_lang)
+        outputs.append(output)
+    nllb_model.cpu()
+    with no_ssl_verification():
+        file_name = tts(outputs, opp_lang)
+    torch.cuda.empty_cache()
+    return text, outputs, file_name
+
+
 @socketio.on('message_from_frontend')
 def handle_message(data):
+    global fn
     message = data["message"]
     fn = data["file_name"]
-
-    print('받은 메시지:', message,fn)
-
-    # 여기서 메시지를 처리하거나 다른 동작을 수행가능
-    # 이걸로 프론트엔드로 통신 가능
-
+    lang = data["language"]
+    opp_lang = lang_li.index(lang)
     timenow = dt.datetime.now().isoformat(timespec="seconds").replace("-", "/").replace("T", "-")
-
-    sleep(1)
-    output=base64.b64encode(message.encode('u8')).decode('u8')
-
+    _, outputs, attachment = song(message, opp_lang)
+    output = " ".join(outputs)
     obj=json.loads((chat_root/f"{fn}.json").read_text('u8'))
     obj["messages"].append(
         {
@@ -43,15 +105,100 @@ def handle_message(data):
         "sender": "R.O.K.N.",
         "content": message,
         "translated": output,
-        "attachment": None
+        "attachment": attachment
         })
     (chat_root/f"{fn}.json").write_text(json.dumps(obj,ensure_ascii=False,indent=2),'u8')
     handle_update_chatlog({"file_name":fn})
-    emit('message_from_backend', {'status': 'success','msg':output})
+
+
+def stt(audio_file):
+    result = whisper_model.transcribe(audio_file)
+    language = result["language"]
+    return result, language
+
+
+def translate_soo(text):
+    inputs = tokenizer_opp(text, return_tensors="pt").to(device)
+    translated_tokens = nllb_model.generate(**inputs, forced_bos_token_id=tokenizer_opp.lang_code_to_id["kor_Hang"], max_length=512)
+    output = tokenizer_kor.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+    return output
+
+
+def soo(file_name):
+    whisper_model.to(device)
+    result, language = stt(file_name)
+    whisper_model.cpu()
+    # if lang_li_whisper.index(language) != opp_lang:
+    #     st.write(f"{lang_li[opp_lang]}가 아닌 다른 언어가 감지됨. \n번역 성능이 저하됩니다. \n올바른 언어를 선택하십시오. ")
+    txts = []
+    for txt in re.split("[.?,。]", result["text"]):
+        if txt.rstrip() == "" or txt.rstrip() == "\n":
+            continue
+        txts.append(txt)
+    nllb_model.to(device)
+    outputs = []
+    for txt in txts:
+        output = translate_soo(txt)
+        outputs.append(output)
+    nllb_model.cpu()
+    torch.cuda.empty_cache()
+    return txts, outputs
+
+
+@socketio.on('audio_upload')
+def handle_audio_upload(data):
+    audio_blob = data["audioBlob"]
+    global fn
+    file_name = secrets.token_urlsafe(8) + ".wav"
+
+    audio_bytes = io.BytesIO(audio_blob)
+    with wave.open('static/audios/' + file_name, 'wb') as wave_file:
+        wave_file.setnchannels(2)
+        wave_file.setsampwidth(2)
+        wave_file.setframerate(48000)
+        wave_file.writeframes(audio_bytes.read())
+
+    timenow = dt.datetime.now().isoformat(timespec="seconds").replace("-", "/").replace("T", "-")
+    message, outputs = soo('static/audios/' + file_name)
+    output = " ".join(outputs)
+    obj=json.loads((chat_root/f"{fn}.json").read_text('u8'))
+    obj["messages"].append(
+        {
+        "timestamp": timenow,
+        "sender": "Other",
+        "content": message,
+        "translated": output,
+        "attachment": file_name
+        })
+    (chat_root/f"{fn}.json").write_text(json.dumps(obj,ensure_ascii=False,indent=2),'u8')
+    handle_update_chatlog({"file_name":fn})
+
+
+@socketio.on('freq_message')
+def handle_freq_message(data):
+    global fn
+    fn = data["file_name"]
+    content = data["content"]
+    translated = data["translated"]
+    attachment = data["attachment"]
+
+    timenow = dt.datetime.now().isoformat(timespec="seconds").replace("-", "/").replace("T", "-")
+    obj=json.loads((chat_root/f"{fn}.json").read_text('u8'))
+    obj["messages"].append(
+        {
+        "timestamp": timenow,
+        "sender": "R.O.K.N.",
+        "content": content,
+        "translated": translated,
+        "attachment": attachment
+        })
+    (chat_root/f"{fn}.json").write_text(json.dumps(obj,ensure_ascii=False,indent=2),'u8')
+    handle_update_chatlog({"file_name":fn})
 
 
 @socketio.on('update_chat_title')
 def update_chat_title(data):
+    global fn
     fn = data["file_name"]
     new_title = data["chat_title"]
     obj=json.loads((chat_root/f"{fn}.json").read_text('u8'))
@@ -62,26 +209,24 @@ def update_chat_title(data):
 
 @socketio.on('req_update_chatlog')
 def handle_update_chatlog(data):
+    global fn
     fn = data["file_name"]
-    emit('update_chatlog',
-         {"file_name":fn,"content":(chat_root/f"{fn}.json").read_text("u8")})
+    emit('update_chatlog', {"file_name":fn,"content":(chat_root/f"{fn}.json").read_text("u8")})
 
 
 @socketio.on('new_com')
 def handle_new_com():
+    global fn
     time = dt.datetime.now().isoformat(timespec="seconds").replace("-", "_").replace(":","_")
     fn = time + "_" + secrets.token_urlsafe(8)
     (chat_root/f"{fn}.json").write_text(f'{{"chat_title": "{time}", "language": "", "messages":[]}}')
-
-    def callback():
-        socketio.emit('load_new_com', fn, namespace='/')
-    
+    socketio.emit('load_new_com', fn)
     socket_send_current_file_list()
-    socketio.start_background_task(callback)
 
 
 @socketio.on('language_select')
 def language_select(data):
+    global fn
     fn = data["file_name"]
     lang = data["language"]
     obj=json.loads((chat_root/f"{fn}.json").read_text('u8'))
@@ -106,4 +251,16 @@ def socket_send_current_file_list():
 
 
 if __name__ == '__main__':
+    lang_li = ("korean", "english", "chinese", "japanese", "russian", "etc")
+    lang_li_whisper = ["ko", "en", "zh", "ja", "ru"]
+    lang_li_nllb = ["kor_Hang", "eng_Latn", "zho_Hans", "jpn_Jpan", "rus_Cyrl"]
+    lang_li_bark = ["ko", "en", "zh", "ja", 'ru']
+
+    chat_root=Path('chat_logs')
+    whisper_model, nllb_model = load_models()
+    tokenizer_kor, tokenizer_opp = None, None
+    opp_lang = None
+    fn = None
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     socketio.run(app, debug=True, port=5500)
--- static/script.js
+++ static/script.js
@@ -8,29 +8,186 @@ document.addEventListener('DOMContentLoaded', function () {
     const filelistEL = document.getElementById('filelist');
     const chatTitleEL = document.getElementById('chat_title');
     const languageSelect = document.getElementById('languageSelect');
-
-    let currentFile = undefined;
+    const chatlogScrollEL = document.getElementById('chatlog-scroll');
+    const waveformCanvas = document.getElementById('waveformCanvas');
+    const canvasContext = waveformCanvas.getContext('2d');
+    const recordButton = document.getElementById('recordButton');
 
     const socket = io.connect('http://localhost:5500');
 
+    let isRecording = false;
+    let recorder;
+    let analyser;
+    // /** @type {MediaStreamAudioSourceNode} */
+    let mediaStreamSource;
+
+    function audioInput() {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(handleSuccess)
+            .catch(handleError);
+    }
+
+    function handleSuccess(stream) {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = audioContext.createAnalyser();
+        mediaStreamSource = audioContext.createMediaStreamSource(stream);
+        mediaStreamSource.connect(analyser);
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        const WIDTH = waveformCanvas.width;
+        const HEIGHT = waveformCanvas.height;
+        const barWidth = (WIDTH / bufferLength) * 2.5;
+        const draw = () => {
+            analyser.getByteTimeDomainData(dataArray);
+            canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
+            canvasContext.beginPath();
+
+            let x = 0;
+            for (let i = 0; i < bufferLength; i++) {
+                const v = (dataArray[i]) / 128.0;
+                const y = (v * HEIGHT) / 2;
+                if (i === 0) {
+                    canvasContext.moveTo(x, y);
+                } else {
+                    canvasContext.lineTo(x, y);
+                }
+                x += barWidth;
+            }
+            canvasContext.lineTo(WIDTH, HEIGHT / 2);
+            canvasContext.stroke();
+            requestAnimationFrame(draw);
+        };
+        draw();
+    }
+
+    function handleError(error) {
+        console.error('마이크 액세스 에러:', error);
+    }
+
+    function toggleRecording() {
+        if (!isRecording) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+        isRecording = !isRecording;
+        const buttonText = isRecording ? '녹음 종료' : '녹음 시작';
+        recordButton.textContent = buttonText;
+        // updateRecordButton();
+    }
+
+    function startRecording() {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(function (stream) {
+                recorder = new Recorder(mediaStreamSource);
+                recorder.record();
+            })
+            .catch(function (err) {
+                console.error('Error accessing microphone:', err);
+            });
+        console.log('Recording started');
+    }
+
+    function stopRecording() {
+        recorder.stop();
+        recorder.exportWAV(function (blob) {
+            // sendAudioToBackend(blob);
+
+            // loading 메세지 띄우기
+            const timenow = (new Date()).toISOString().replace(/-/g, '/').replace('T', '-').replace(/\.\d{3}Z$/, '');
+            const newChat = createChat({ sender: "Other", timestamp: timenow, content: "Loading", translated: "Loading" });
+            chatlogEL.appendChild(newChat);
+            chatlogScrollEL.scrollTop = chatlogScrollEL.scrollHeight;
+
+            socket.emit('audio_upload', { "audioBlob": blob });
+        });
+        console.log('Recording stopped');
+    }
+
+    // function sendAudioToBackend(audioBlob) {
+    //     // 여기에 녹음된 파일을 전송하는 코드 추가 (예: Ajax 요청 등)
+    //     const formData = new FormData();
+    //     formData.append('audioData', audioBlob);
+    //     formData.append("sid", socket.id);
+    //     fetch('/upload', {
+    //         method: 'POST',
+    //         body: formData,
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log('Audio uploaded successfully:', data);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error uploading audio:', error);
+    //         });
+
+    //     updateRecordButton();
+    // }
+
+    // function updateRecordButton() {
+    //     const buttonText = isRecording ? '녹음 종료' : '녹음 시작';
+    //     recordButton.textContent = buttonText;
+    // }
+
+    // let pendingScrollOnDraw = false;
+    let pendingUpdateChatlogJobs = [];
+    let currentFile = undefined;
+    let initialLoad = true;
+
     /**
-     * 
      * @param {string} file_name 
      */
     function loadChat(file_name) {
+        audioInput();
         currentFile = file_name;
         drawChat();
         socket.emit('req_update_chatlog', { file_name });
         console.log(file_name, "Clicked!");
 
-        const chatlog_raw = localStorage.getItem(currentFile);
-        if (chatlog_raw != null) {
-            const chat_log = JSON.parse(chatlog_raw);
-            const selectedLanguage = chat_log["language"];
+        pendingUpdateChatlogJobs.push(() => {
+            const chatlog_raw = localStorage.getItem(currentFile);
+            console.log(currentFile);
+            console.log(chatlog_raw);
+            if (chatlog_raw != null) {
+                const chat_log = JSON.parse(chatlog_raw);
+                const selectedLanguage = chat_log["language"];
+                console.log(selectedLanguage);
+                languageSelect.value = selectedLanguage;
+                if (selectedLanguage != "") {
+                    socket.emit('load_tokenizer', { selectedLanguage });
+                }
+                loadFreqUsedScripts(selectedLanguage);
+            };
+        })
 
-            languageSelect.value = selectedLanguage;
-            loadFreqUsedScripts(selectedLanguage);
+        if (!initialLoad) {
+            document.getElementById('chat_title').style.display = 'block';
+            document.getElementById('languageLabel').style.display = 'block';
+            document.getElementById('languageSelect').style.display = 'block';
+            document.getElementById('chatbox').style.visibility = 'visible';
+            document.getElementById('mic_input_control').style.visibility = 'visible';
+            document.getElementById('chatlog-scroll').style.visibility = 'visible';
+        };
+    };
+
+    function drawChat() {
+        const chatlog_raw = localStorage.getItem(currentFile);
+        if (chatlog_raw == null) return;
+        else {
+            const chat_log = JSON.parse(chatlog_raw);
+            chatTitleEL.innerText = chat_log["chat_title"];
+            chatlogEL.innerHTML = "";
+            chat_log["messages"].forEach((v, i) => {
+                const newChat = createChat(v);
+                chatlogEL.appendChild(newChat);
+            })
         }
+        chatlogScrollEL.scrollTop = chatlogScrollEL.scrollHeight;
+        // if (pendingScrollOnDraw) {
+        //     pendingScrollOnDraw = false;
+        //     chatlogScrollEL.scrollTop = chatlogScrollEL.scrollHeight;
+        // }
     }
 
     function editChatTitle() {
@@ -51,29 +208,25 @@ document.addEventListener('DOMContentLoaded', function () {
         chatTitleEL.focus();
     }
 
-    function drawChat() {
-        const chatlog_raw = localStorage.getItem(currentFile);
-        if (chatlog_raw == null) return;
-        else {
-            const chat_log = JSON.parse(chatlog_raw);
-            chatTitleEL.innerText = chat_log["chat_title"];
-            chatlogEL.innerHTML = "";
-            chat_log["messages"].forEach((v, i) => {
-                const newChat = createChat(v);
-                chatlogEL.appendChild(newChat);
-            })
+    function sendMessage() {
+        const message = messageInput.value;
+        if (languageSelect.value == "") {
+            alert("언어를 선택하십시오!");
+            return;
+        }
+        if (message.trim() !== '') {
+            // pendingScrollOnDraw = true;
+            const timenow = (new Date()).toISOString().replace(/-/g, '/').replace('T', '-').replace(/\.\d{3}Z$/, '');
+            const newChat = createChat({ sender: "R.O.K.N.", timestamp: timenow, content: message, translated: "Loading" });
+            chatlogEL.appendChild(newChat);
+            chatlogScrollEL.scrollTop = chatlogScrollEL.scrollHeight;
+            socket.emit('message_from_frontend', { "file_name": currentFile, "language": languageSelect.value, "message": message });
+            messageInput.value = '';
         }
     }
 
-    function sendMessage() {
-        const message = messageInput.value;
-        if (message.trim() !== '') {
-            const timenow = (new Date()).toISOString();
-            const newChat = createChat({ sender: "R.O.K.N.", timestamp: timenow, content: message, translated: "Loading" });
-            chatlogEL.appendChild(newChat);
-            socket.emit('message_from_frontend', { "file_name": currentFile, "message": message });
-            messageInput.value = '';
-        }
+    function sendFreqMessage(content, translated, attachment) {
+        socket.emit('freq_message', { "file_name": currentFile, "content": content, "translated": translated, "attachment": attachment });
     }
 
     function loadFreqUsedScripts(selectedLanguage) {
@@ -96,35 +249,45 @@ document.addEventListener('DOMContentLoaded', function () {
                     scripts.forEach((script, index) => {
                         const scriptDiv = document.createElement('div');
                         scriptDiv.innerText = script.content;
+                        scriptDiv.addEventListener('click', () => {
+                            // pendingScrollOnDraw = true;
+                            sendFreqMessage(script.content, script.translated, script.attachment);
+                        });
                         rightSidebarEL.appendChild(scriptDiv);
                     });
                 });
-        }
-    }
+        };
+    };
 
     function handleKeyPress(event) {
         if (event.key === 'Enter' && !event.shiftKey) {
             event.preventDefault();
             sendMessage();
-        }
-    }
+        };
+    };
 
     function start_new_com() {
         socket.emit('new_com');
-    }
+    };
 
     function select_language() {
         let selectedLanguage = languageSelect.value;
+        socket.emit('load_tokenizer', { selectedLanguage })
         socket.emit('language_select', { "file_name": currentFile, "language": selectedLanguage });
         loadFreqUsedScripts(selectedLanguage);
-    }
-
-    socket.on('message_from_backend', function (data) {
-        console.log('받은 메시지:', data);
-    });
+    };
 
     socket.on('file_list_update', function (data) {
-        console.info('받은 메시지:', data);
+        if (initialLoad) {
+            document.getElementById('chat_title').style.display = 'none';
+            document.getElementById('languageLabel').style.display = 'none';
+            document.getElementById('languageSelect').style.display = 'none';
+            document.getElementById('chatbox').style.visibility = 'hidden';
+            document.getElementById('mic_input_control').style.visibility = 'hidden';
+            document.getElementById('chatlog-scroll').style.visibility = 'hidden';
+            initialLoad = false;
+        };
+
         const { file_list } = data;
 
         filelistEL.innerHTML = "";
@@ -134,17 +297,19 @@ document.addEventListener('DOMContentLoaded', function () {
             file_div.innerText = file["chat_title"];
             file_div.addEventListener('click', () => loadChat(file["file_name"]));
             filelistEL.appendChild(file_div);
-        })
-    })
+        });
+    });
 
     socket.on('load_new_com', function (fn) {
         loadChat(fn);
-    })
+    });
 
     socket.on('update_chatlog', function (data) {
         localStorage.setItem(data["file_name"], data["content"]);
+        pendingUpdateChatlogJobs.forEach(v => v())
+        pendingUpdateChatlogJobs = [];
         if (currentFile == data["file_name"]) drawChat();
-    })
+    });
 
     socket.on('chat_title_bug', function () {
         chatTitleEL.click();
@@ -156,4 +321,5 @@ document.addEventListener('DOMContentLoaded', function () {
     newComEL.addEventListener('click', start_new_com);
     chatTitleEL.addEventListener('click', editChatTitle);
     languageSelect.addEventListener('change', select_language);
+    recordButton.addEventListener('click', toggleRecording);
 });
